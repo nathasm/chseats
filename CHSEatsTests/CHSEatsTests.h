2d3e5733e99672f262f49271e1a2928b0da8419e
Nathan Smith <nathasm@gmail.com>
2012-06-04 18:27:41 -0400

    Initial Commit

---
+++ CHSEatsTests/CHSEatsTests.h
@@ -0,0 +1,13 @@
+//
+//  CHSEatsTests.h
+//  CHSEatsTests
+//
+//  Created by ernest andrade on 6/4/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface CHSEatsTests : SenTestCase
+
+@end
---
+++ CHSEatsTests/CHSEatsTests.m
@@ -0,0 +1,32 @@
+//
+//  CHSEatsTests.m
+//  CHSEatsTests
+//
+//  Created by ernest andrade on 6/4/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "CHSEatsTests.h"
+
+@implementation CHSEatsTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in CHSEatsTests");
+}
+
+@end
